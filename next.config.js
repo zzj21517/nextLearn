9233e29e5a1ad6d95b0918c8ed2c9ba43869de19
zzj <215179993@qq.com>
2020-07-02 00:05:18 +0800

    clean-webpack-plugin问题解决

--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
-const path = require('path')
 const {
   CleanWebpackPlugin
 } = require('clean-webpack-plugin')
-const {
-  resolve
-} = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path')
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  plugins: [
-    new BundleAnalyzerPlugin(),
-    new CleanWebpackPlugin({
-      dry:true,
-      verbose:true,
-      cleanOnceBeforeBuildPatterns:[]
-    }) //所要清理的文件夹名称
-  ]
+  webpack: (config, {
+    buidId,
+    dev,
+    isServer,
+    defaultLoaders,
+    webpack
+  }) => {
+    // console.log(config.plugins, 'config')
+    config.plugins.unshift(new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [`!${path.join(process.cwd(),'.next/server/pages-manifest.json')}`]
+    }))
+    return config
+  }
 }
